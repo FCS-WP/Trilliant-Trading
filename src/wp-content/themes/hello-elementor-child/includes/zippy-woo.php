e932554bba9ee1191f627f80f3389fce32d2f2f1
DevTran <tan.tran@floatingcube.com>
2024-11-05 17:09:58 +0700

    fix cart sidebar

--- src/wp-content/themes/hello-elementor-child/includes/zippy-woo.php
+++ src/wp-content/themes/hello-elementor-child/includes/zippy-woo.php
@@ -4,3 +4,5 @@
 if (function_exists("register_sidebar")) {
     register_sidebar();
   }
+
+function enqueue_wc_cart_fragments() { wp_enqueue_script( 'wc-cart-fragments' ); } add_action( 'wp_enqueue_scripts', 'enqueue_wc_cart_fragments' );
